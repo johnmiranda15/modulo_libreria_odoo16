3dcf26a532343557cedf429f19730fc668f0b766
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-17 15:38:11 -0500

    se actualiza el modelo libros a libros_biblioteca, se crean nuevos campos de para asignaciones de libros a usuarios

--- models/libros_biblioteca.py
+++ models/libros_biblioteca.py
@@ -1,8 +1,11 @@
-from odoo import models, fields
+"""libros"""
+
+from odoo import models, fields, api
 
 #Creando un modelo ( tabla de datos) a partir de una clase
 class Libros(models.Model):
-    _name = 'libros' #nombre de la tabla que se va a generar
+    """Clase Libros Biblioteca"""
+    _name = 'libros.biblioteca' #nombre de la tabla que se va a generar
     _description = 'Informacion de cada libro'
     _inherit = ['mail.thread','mail.activity.mixin']
                 
@@ -10,9 +13,15 @@ class Libros(models.Model):
     editorial = fields.Char(string="Editorial", required=True)
     isbn = fields.Char(string="ISBN", required=True)
     autor_id = fields.Many2one(comodel_name="autor", string="Autor", required=True)
-    lastname_autor = fields.Char(related="autor_id.last_name", string="Apellido del Autor") 
+    lastname_autor = fields.Char(related="autor_id.last_name", string="Apellido del Autor")
     image = fields.Binary(string="Image")
+    fecha_publicacion = fields.Date(string="Fecha de Publicación")
     categoria_id = fields.Many2one(comodel_name='categoria.libro')
+    #campos de asignacion de libros
+    hermano = fields.Many2one(comodel_name='usuarios.libros', string="Hermano(a)")
+    fecha_entrega = fields.Date(string="Fecha de Entrega")
+    fecha_recibido = fields.Date(string="Fecha de Recibido")
+    dias_transcurridos = fields.Integer(string="Días Transcurridos", compute="_compute_dias_transcurridos")
     state = fields.Selection([
         ('available', 'Disponible'),
         ('reserved', 'Reservado'),
@@ -35,6 +44,14 @@ class Libros(models.Model):
     
     def btn_prestado(self):
         self.write({'state': 'loaned'})
+
+    @api.depends('fecha_entrega', 'fecha_recibido')
+    def _compute_dias_transcurridos(self):
+        for record in self:
+            if record.fecha_entrega and record.fecha_recibido:
+                record.dias_transcurridos = (record.fecha_recibido - record.fecha_entrega).days
+            else:
+                record.dias_transcurridos = 0
 
 class CategoriaLibro(models.Model):
     _name ='categoria.libro'
