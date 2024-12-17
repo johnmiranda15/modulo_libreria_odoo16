4a3e78ab0deb947ed03d00fbc2a526bc1994c04a
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-17 15:21:37 -0500

    se actualiza el modelo con nuevos campos de fecha de nacimiento

--- models/author.py
+++ models/author.py
@@ -1,9 +1,11 @@
 from odoo import models, fields
 
 class Autor(models.Model):
+    """Clase Autor"""
     _name = 'autor'
     _description = 'Informacion del autor del libro'
-    _rec_name = 'last_name'
+    _rec_name = 'name'
     
     name = fields.Char(string="Nombre del Autor: ", required=True)
     last_name = fields.Char(string="Apellido del Autor")
+    fecha_nacimiento = fields.Date(string="Año de Nacimiento")
