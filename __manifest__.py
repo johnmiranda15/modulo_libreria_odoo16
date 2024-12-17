3dae222d38a55cf59cb1fe3c04398bea371e381f
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-17 15:44:19 -0500

    se agregan las nuevas vistas xml al manifest

--- __manifest__.py
+++ __manifest__.py
@@ -7,11 +7,15 @@
     'version': '1.0.0',
     'depends': ['mail'],
     'data': [
-        'views/menu_view.xml',
-        'views/libros_view.xml',
+        'views/libros_biblioteca_view.xml',
+        'views/pedido_libros_view.xml',
+        'views/autores_view.xml',
+        'views/accion_servidor_importar_documento.xml',
+        'views/importar_documento_view.xml',
+        'views/usuarios_view.xml',
         'security/libreria_security.xml',
         'security/ir.model.access.csv',
-        'report/libros_report.xml'
+        #'report/libros_report.xml'
     ],
     'images': ['static/description/icon.png']
 }
