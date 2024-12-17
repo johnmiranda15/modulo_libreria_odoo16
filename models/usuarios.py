02fff01e3a46a4e63e3675cb837ffcd5827a42e0
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-17 15:41:54 -0500

    se crea el modelo usuarios_libros para registrar a cada usuario con sus detalles

--- models/usuarios.py
+++ models/usuarios.py
@@ -1,4 +1,14 @@
-"""hermanos"""
+"""usuarios"""
 
 
-from
+from odoo import models, fields
+
+class Hermanos(models.Model):
+    """Clase Hermanos"""
+    _name = 'usuarios.libros'
+    _description = "Modelo de Usuarios que podrán adquirir libros"
+    _rec_name = 'nombre_hermano'
+
+    nombre_hermano = fields.Char(string="Hermano(a)")
+    celular = fields.Integer(string="Celular")
+    direccion = fields.Char(string="Dirección")
